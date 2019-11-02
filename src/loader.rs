320f9815ab8617a7a095aa82aabacfec73a2c954
Jim Wiegand <jwiegand@mobilehealth.net>
2019-11-02 08:16:08 -0400

    Next step

--- src/loader.rs
+++ src/loader.rs
@@ -1,19 +1,21 @@
 
-pub fn loader() -> &'static Vec<Point> {
-    let mut points: Vec<Point> = Vec::new();
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+}
+
+
+pub fn loader<'a>(points: &'a Vec<Point>) -> &'a Vec<Point> {
+    
     points.push(new_point(1,1));
     points.push(new_point(2,1));
     points.push(new_point(3,1));
     points.push(new_point(1,2));
     points.push(new_point(2,2));
     points.push(new_point(3,2));
-    &points
+    return points;
 }
-#[derive(Copy, Clone)]
-pub struct Point {
-    pub x: i32,
-    pub y: i32,
-}
+//#[derive(Copy, Clone)]
 pub fn new_point(x:i32, y:i32) -> Point{
     return Point {x,y,};
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,37 @@
 mod pause;
-mod loader;  
+//mod loader;  
+//use loader::{loader, find_rec};
 
 use pause::pause;
-use loader::{loader, find_rec};
 
+struct Point {
+    pub x: i32,
+    pub y: i32,
+}
 
 fn main() {
     
+    let mut points: Vec<Point> = Vec::new();
+
     println!("Ready to go?");
     pause();
     
-    let points = loader();
+//    loader(&points);
+    points.push(Point {x:1,y:1});
+    points.push(Point {x:2,y:1});
+    points.push(Point {x:3,y:1});
+    points.push(Point {x:1,y:2});
+    points.push(Point {x:2,y:2});
+    points.push(Point {x:3,y:2});
 
+    
     // Show points
     for p in points {
         println!("x:{}, y:{}",p.x,p.y);    
+        //points.contains(x: &T)
     }
 
-    //let count: u32 = find_rec(points);
-    //println!("Count is {}",count);
+    
 
 }
+    
